0442445be719340201290688e432cf62768d1d93
김영훈 <hunifive@gmail.com>
2025-07-19 23:54:34 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,15 +1,8 @@
 class Solution {
     public String solution(String s) {
-        String[] sArr = s.split("");
-        
-        double center = (double) (s.length() - 1) / 2;
-        
-        if (center % 1 == 0) {
-            return sArr[(int) center];
-        } else {
-            int before = (int) Math.floor(center);
-            int after = (int) Math.ceil(center);
-            return sArr[before] + sArr[after];
-        }
+        int first = (s.length() - 1) / 2;
+        int second = s.length() / 2 + 1;
+
+        return s.substring(first, second);
     }
 }
