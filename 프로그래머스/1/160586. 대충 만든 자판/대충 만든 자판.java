254ccce4f44ce77b25ccb79a6fa7c1cf547a2508
김영훈 <hunifive@gmail.com>
2025-08-25 10:19:22 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.38 ms, Memory: 93.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = new int[targets.length];
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (String key : keymap) {
+            for (int j = 0; j < key.length(); j++) {
+                char ch = key.charAt(j);
+
+                if (map.containsKey(ch)) {
+                    if (map.get(ch) > j) map.replace(ch, j + 1);
+                } else {
+                    map.put(ch, j + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < targets.length; i++) {
+            int sum = 0;
+
+            for (int j = 0; j < targets[i].length(); j++) {
+                char ch = targets[i].charAt(j);
+
+                if (map.containsKey(ch)) {
+                    sum += map.get(ch);
+                } else {
+                    sum = -1;
+                    break;
+                }
+            }
+
+            answer[i] = sum;
+        }
+
+        return answer;
+    }
+}
